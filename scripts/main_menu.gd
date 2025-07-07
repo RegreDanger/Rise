2e4e5f9ffec815e79112767e4df6feddf3b5ca6b
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-07-07 17:37:11 -0400

    add inventory ui and level one.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -4,13 +4,15 @@ extends Control
 
 #imports 
 @onready var exit_button = $VBoxContainer/MarginContainer/VBoxContainer/ExitButton
+@onready var play_button = $VBoxContainer/MarginContainer/VBoxContainer/PlayButton
 
 func _ready() -> void:
-	print("ESTOY VIVOOOOOOOOOOOOOO!!!")
-	
 	#connect events 
+	play_button.connect("pressed",on_play_pressed)
 	exit_button.connect("pressed",on_exit_pressed)
 
+func on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/levels/level_one.tscn")
 
 func on_exit_pressed():
 	get_tree().quit()
---
+++ scripts/ui/inventory.gd
@@ -0,0 +1,40 @@
+class_name Inventory
+
+extends Control
+
+#ready 
+@onready var itemList = $ItemList
+
+
+func _ready() -> void:
+	#var file = FileAccess.open("res://data/user.dat.txt", FileAccess.READ)
+	#var content = file.get_as_text()
+	
+	add_fake_data()
+
+func _process(delta: float) -> void:
+	var key_i_pressed: bool = Input.is_action_just_pressed("key_i")
+	
+	if key_i_pressed: 
+		if itemList.is_visible_in_tree():
+			itemList.hide()
+		else:
+			itemList.show()
+
+func add_fake_data():
+	itemList.add_item("23",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("13",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("2",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("3",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("22",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	itemList.add_item("1",load("res://assets/sprites/player/SDMS-s.png"))
+	
+	
+	
+	
