c5874b57e99d5c38c0f917e12e980396f9971b1f
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-07-08 09:07:11 -0400

    Refactor inventory management and UI components for improved functionality and organization

--- scripts/autoload/input/input_manager.gd
+++ scripts/autoload/input/input_manager.gd
@@ -1,3 +1,5 @@
+class_name Input_manager
+
 extends Node
 
 # global signal
--- scripts/ui/inventory.gd
+++ scripts/ui/inventory.gd
@@ -1,16 +1,14 @@
 class_name Inventory extends Control
 
 #readys
-@onready var input_manager_loaded = input_manager
-@onready var inventory = $"."
-@onready var itemList = $HBoxContainer/VBoxContainer2/ItemList
+@onready var input_manager_loaded: Input_manager = input_manager
+@onready var inventory: Control = $"."
+@onready var itemList: ItemList = $HBoxContainer/VBoxContainer2/ItemList
 
 func _ready() -> void:
+	#events 
 	input_manager_loaded.connect("input_action", _show_inventory)
 	add_fake_data()
-
-func _process(delta: float) -> void:
-	pass
 
 func _show_inventory(action: String, toggle: bool):
 	match action:
@@ -24,5 +22,3 @@ func _show_inventory(action: String, toggle: bool):
 func add_fake_data():
 	for i in range(120):
 		itemList.add_item(str(i + 1),load("res://assets/sprites/player/SDMS-s.png"))
-	
-	
