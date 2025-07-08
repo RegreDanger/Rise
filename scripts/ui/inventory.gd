34da4aa36d2404ffa0726693545b11e78e26d74e
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-07-07 21:49:42 -0400

    Refactor inventory management and UI interactions for improved functionality

--- scripts/ui/inventory.gd
+++ scripts/ui/inventory.gd
@@ -2,28 +2,24 @@ class_name Inventory
 
 extends Control
 
-#ready 
+#readys
+@onready var inventory = $"."
 @onready var itemList = $HBoxContainer/VBoxContainer2/ItemList
 
 func _ready() -> void:
-	#var file = FileAccess.open("res://data/user.dat.txt", FileAccess.READ)
-	#var content = file.get_as_text()
-	
 	add_fake_data()
 
 func _process(delta: float) -> void:
 	var key_i_pressed: bool = Input.is_action_just_pressed("key_i")
 	
 	if key_i_pressed: 
-		if itemList.is_visible_in_tree():
-			itemList.hide()
+		if inventory.is_visible_in_tree():
+			inventory.hide()
 		else:
-			itemList.show()
+			inventory.show()
 
 func add_fake_data():
 	for i in range(120):
 		itemList.add_item(str(i + 1),load("res://assets/sprites/player/SDMS-s.png"))
-	
-	
 	
 	
