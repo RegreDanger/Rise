d2e9047632362b22deb011761f556af51fa07488
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-07-09 22:52:46 -0400

    Refactor editor layout and introduction script for improved functionality and organization. Updated selected script and adjusted editor state settings.

--- scripts/ui/introduction.gd
+++ scripts/ui/introduction.gd
@@ -10,16 +10,17 @@ const dialog = [
 #ready 
 @onready var input_manager_loaded: Input_manager = input_manager
 @onready var label: Label = $CanvasLayer/Control/PanelContainer/MarginContainer/Label
+
 #vars 
-var current_line = 0
+var current_line: int = 0
 
 func _ready() -> void:
 	label.text = dialog[current_line]
-	current_line = 1
+	current_line += 1
 	
 	#events 
 	input_manager_loaded.connect("input_action", next_line)
-	
+
 func next_line(action: String, toggle: bool):
 	if action == "key_space" || "click":
 		if toggle && dialog.size() > current_line:
