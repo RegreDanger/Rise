ea8a8790b5e81347f3ef9b1137fa6d3327262c1a
RegreDanger <carlosemiliogranadaperez@outlook.com>
2025-07-15 05:48:28 -0600

    Added: New sprite, new animations and directions
    - Added new sprite
    - Added animation by frames
    - Refactored player sprite direction
    - Added rotation animation in procedural_animations script

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,19 +1,55 @@
 class_name player extends CharacterBody2D
 
-var move_speed_base = 150;
+var cardinal_direction: Vector2 = Vector2.DOWN
+var direction: Vector2 = Vector2.ZERO
+var move_speed_base = 150
+var procedural_animations_loaded = procedural_animations.new()
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite: Sprite2D = $Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	add_child(procedural_animations_loaded)
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var direction: Vector2 = Vector2.ZERO
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	velocity = direction * move_speed_base 
-	pass
+	velocity = direction * move_speed_base
+	if _set_direction() == true:
+		_update_animation()
 
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+
+func _set_direction() -> bool:
+	var new_dir: Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+	
+	if new_dir == cardinal_direction:
+		return false
+	
+	cardinal_direction = new_dir
+	match cardinal_direction:
+		Vector2.UP:
+			procedural_animations_loaded.rotate_to(0, sprite)
+		Vector2.DOWN:
+			procedural_animations_loaded.rotate_to(180, sprite)
+		Vector2.LEFT:
+			procedural_animations_loaded.rotate_to(-90, sprite)
+		Vector2.RIGHT:
+			procedural_animations_loaded.rotate_to(90, sprite)
+
+	return true
+
+func _update_animation() -> void:
+	animation_player.play("movement")
+	
--- scripts/utils/procedural_animations.gd
+++ scripts/utils/procedural_animations.gd
@@ -1,5 +1,6 @@
 class_name procedural_animations extends Node
 var tween_typwritter: Tween = null
+var tween_rotation: Tween = null
 
 func typewritter(time: float, text: String, node: Node) -> void:	
 	if "text" in node:
@@ -9,3 +10,14 @@ func typewritter(time: float, text: String, node: Node) -> void:
 		node.text = text
 		tween_typwritter = create_tween()
 		tween_typwritter.tween_property(node, "visible_ratio", 1.0, time)
+
+func rotate_to(target_angle: float, sprite: Sprite2D) -> void:
+	if tween_rotation and tween_rotation.is_valid():
+		tween_rotation.kill()
+		
+	var current_rot = sprite.rotation
+	var target_rot = deg_to_rad(target_angle)
+	var shortest_rot = wrapf(target_rot - current_rot, -PI, PI) + current_rot
+	
+	tween_rotation = create_tween()
+	tween_rotation.tween_property(sprite, "rotation", shortest_rot, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
